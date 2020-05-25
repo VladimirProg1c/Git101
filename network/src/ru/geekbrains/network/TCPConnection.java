68632571b5399eb8eb706be4ca4fbae9f44e658c
Vova <Vladimirprog1c@gmail.com>
2020-05-25 17:22:36 +0300

    test2

---
+++ network/src/ru/geekbrains/network/TCPConnection.java
@@ -0,0 +1,85 @@
+package ru.geekbrains.network;
+
+//import sun.rmi.rmic.IndentingWriter;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class TCPConnection {
+
+    private final Socket socket;
+    private final Thread rxThread;
+    private final TCPConnectionListener eventListener;
+    private final BufferedReader in;
+    private final BufferedWriter out;
+
+    public TCPConnection(TCPConnectionListener eventListener, String ipAdr, int port) throws IOException {
+
+      this(eventListener, new Socket(ipAdr, port));
+
+    }
+
+    public TCPConnection(TCPConnectionListener eventListener, Socket socket) throws IOException {
+
+        this.eventListener = eventListener;
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), Charset.forName("UTF-8")));
+
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), Charset.forName("UTF-8")));
+
+        rxThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    eventListener.onConnectionReady(TCPConnection.this);
+                    while (!rxThread.isInterrupted()){
+                        String msq = in.readLine();
+                        eventListener.onReceiveString(TCPConnection.this, msq);
+
+                    }
+                    String msq = in.readLine();
+                } catch (IOException e) {
+                    eventListener.onException(TCPConnection.this, e);
+                }
+                finally {
+                    eventListener.onDisconnect(TCPConnection.this);
+                }
+
+            }
+        });
+        rxThread.start();
+
+    }
+
+    public synchronized void  sendString(String value){
+
+        try {
+            out.write(value + "\r\n"); //возврат корретки  и перевод строки
+            out.flush(); //принудительно отправляем всё, что могло зависнуть в буфере до лучших времён
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+            disconnect();
+
+        }
+
+    }
+
+    public synchronized void  disconnect(){
+
+        rxThread.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this, e);
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "TCPConnection: " + socket.getInetAddress() + ": " + socket.getPort();
+    }
+}
